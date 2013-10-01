cdadab9196df69d7b3e4ebe19a21eec169a81f1a
Peter Chen <pchen@4-south.com>
2013-10-01 01:15:40 -0500

    cleanup and comments

--- public/dist/app.js
+++ public/dist/app.js
@@ -10,17 +10,21 @@ App.Item = Ember.Object.extend()
 
 
 App.ItemListView = Em.CollectionView.extend({
-  didInsertElement: function(){console.log("fu")},
   tagName: 'ul',
   classNames: ['draggable'],
   itemViewClass: JQ.DragView.extend({
+    //setup
     templateName: 'item',
     classNames: ['box'],
     revert: 'invalid',
+    //helpers
+    serializeData: function(model){
+      return JSON.stringify(model);
+    },
     start: function(event, ui){
       //Grabs the Content and converts to JSON, then attaches the data
-      var model = JSON.stringify(this.get('content'));
-      this.$().data("model", model);
+      var dataString = this.serializeData(this.get('content');
+      this.$().data("model", dataString);
     },
 
   }),
@@ -31,28 +35,29 @@ App.ItemListView = Em.CollectionView.extend({
 })
 
 App.ItemDropView = JQ.DropView.extend({
+  //settings / setup
   content: [],
   templateName: 'dropzone',
   tagName: 'section',
   classNames: ['dropZone'],
   accept: '.box',
   revert: 'invalid',
+  
+  //events and helper methods
+  deserializeModel: function(dataString){
+    var model;
+    return model = App.Item.create(JSON.parse(dataString));
+  },
   drop: function( event, ui ) {
-      var model = null;
       if(ui){
-        model = JSON.parse($(ui.helper.context).data('model'));
-        model = App.Item.create(model);
-        }
-      if(model)
-      {
-        window.cont = this.get('content');
-        cont.pushObject(model);
-        this.set('content', cont);
+        //convert data from string back into Ember object
+        model = this.deserializeModel($(ui.helper.context).data('model'));
+        this.get('content').pushObject(model);
       }
   }
 })
 
+//component test, doesnt do anything atm
 App.DragDropComponent = Em.Component.extend({
-  testAttr: "hi",
   didInsertElement: function(){console.log("dragComp")}
 })
